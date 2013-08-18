83cd7fd443ce5bf5935b55def0b1300d9aaf3713
Alexandre Anastassov <a.anastassov@iaea.org>
2013-08-18 16:17:19 +0200

    after Resources And Network Panels module

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ chrome-dev-tools
 
 Code based on Using The Chrome Developer Tools with John Sonmez (pluralsight.com)
 
-## Commands ##
+# Overview and Console ##
 
 	// Logging
 	console.log("<message>");
@@ -64,9 +64,35 @@ Code based on Using The Chrome Developer Tools with John Sonmez (pluralsight.com
 	// Other commands
 	clear()
 
+# Elements Pannel ##
+
+	// Shortcuts
+	$n 						- where n=[0..4]; selects previously inspected element
+
+# Resources And Network Panels #
+	LoadResourceData() 		- console: load all resources
+
+	- WebSQL
+		Note: WebSQL is pritty much obsolette
+		on database level SQL can be used
+	- IndexedDB
+	- Local and Session Storage
+	- Cookies
+		Note: To test cookie using local file with with chrome you have to enable them;
+		      option '--enable-file-cookies'
+	- Application Cache
+
+	Tip: It is useful to check 'Disable cache (while DevTools is open)' (see Settings) 
+	     when analyzing Network trafic
+
+# Sources and Audit Panels #
+
+# Miscellaneous ##
+
 ## Documentation ##
 
 - [Chrome DevTools](https://developers.google.com/chrome-developer-tools/)
+- [The Chromium Projects](http://www.chromium.org/getting-involved/dev-channel)
 
 ## Related articles ##
 
@@ -77,3 +103,13 @@ Code based on Using The Chrome Developer Tools with John Sonmez (pluralsight.com
 
 - [Ten cool Chrome tricks you can use right now](http://www.techrepublic.com/blog/google-in-the-enterprise/ten-cool-chrome-tricks-you-can-use-right-now/)
 - [Google Chrome Shortcuts](http://chromecheat.blogspot.co.at/2008/09/google-chrome-shortcuts.html)
+- [Cookbook Demo](http://ie.microsoft.com/testdrive/HTML5/Cookbook/) - leverage HTML5 AppCache and IndexedDB
+
+## Other ##
+
+	// SublimeText 2
+	Use Shift-Ctrl-[ to fold code
+
+## About the Author ##
+
+John Sonmez is the founder of [Simple Programmer](http://simpleprogrammer.com/), a Pluralsight Author and Programmer, focused on making the complex simple.
--- js/doIt.js
+++ js/doIt.js
@@ -12,6 +12,7 @@ function doIt() {
 	console.timeEnd("Steps time");
 
 	useAssert();
+	LoadResourceData();
 }
 
 function stepOne() {
@@ -37,4 +38,68 @@ function useAssert() {
 		console.assert(10 == 5 * 2);	// OK
 		console.assert(10 == 3 * 2);	// fail
 	console.groupEnd();
+}
+
+
+function LoadResourceData() {
+	CreateSQLData();
+	CreateIndexedDbData();
+	CreateLocalAndSessionStorageData();
+	CreateCookieData();
+}
+
+function CreateSQLData() {
+	var db = openDatabase('testdb', '1.0', 'test database', 2 * 1024 * 1024);
+	db.transaction(function (tx) {
+		tx.executeSql('CREATE TABLE text(id unique, data)');
+		tx.executeSql('INSERT INTO text(id, data) VALUES (1, "text")');
+	});
+}
+
+function CreateIndexedDbData() {
+	console.group('IndexDB');
+		var db;
+		var request = indexedDB.open('IndexDB Test DB', 7);
+		
+		request.onerror = function(event) {
+			console.log("Coulden't use IndexDB");
+		};
+		request.onsuccess = function (event) {
+			db = request.result;
+			console.log("Opened database!");
+		};
+
+		var customerData = [
+			{id: 1, name: "Joe"},
+			{id: 2, name: "Bob"}
+		];
+
+		request.onerror = function(event) {
+		};
+		request.onupgradeneeded = function(event) {
+			console.log("Upgrading database");
+			var db = event.target.result;
+
+			if (db.objectStoreNames.contains("customers")) {
+				db.deleteObjectStore("customers");
+			}
+
+			var objectStore = db.createObjectStore("customers", { keyPath: "id"});
+			objectStore.createIndex("name", "name", { unique: false});
+
+			for (var i in customerData) {
+				console.log("Adding customer: %s", i.name);
+				objectStore.add(customerData[i]);
+			}
+		}
+	console.groupEnd('IndexDB');
+}
+
+function CreateLocalAndSessionStorageData() {
+	localStorage.setItem("test key", "test value");
+	sessionStorage.setItem("test session key", "test session value");
+}
+
+function CreateCookieData() {
+	document.cookie = "cookiekey = cookievalue";
 }
